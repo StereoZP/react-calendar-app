2b9e38a0e4714a2782107f14fb62d68e741d57f6
Alexander <sumry.stereo123@ukr.net>
2023-03-20 12:33:46 +0200

    add data.json

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Month from "./Month";
-import {add, format, isSameDay} from "date-fns";
+import {add, format, isSameDay, parseISO} from "date-fns";
 import classes from "./Calendar.module.css";
 import {MyContext} from "../context";
 import EventList from "./EventList";
 import EventTitle from "./EventTitle";
+
+
 
 
 const Calendar = () => {
@@ -16,55 +18,81 @@ const Calendar = () => {
     const [selected, setSelected] = useState(startDay)
 
     setInterval(() => {
-        setDay(new Date())
-    }, 1000)
+         setDay(new Date())
+     }, 1000)
 
     const prev = () => {
-        setMonth(add(month,{months:-1}))
+        setMonth(add(month, {months: -1}))
     }
 
     const next = () => {
-        setMonth(add(month,{months:1}))
+        setMonth(add(month, {months: 1}))
     }
 
-    const [event, setEvent] = useState([{id: Date.now(), date:new Date(), title:'First event'}]);
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [event, setEvent] = useState([]);
 
     const createEvent = (newPost) => {
         setEvent([...event, newPost])
     }
 
-    const usersByLikes = event.map((item, index) => {
-        if(isSameDay(selected, item.date)){
-            return <EventTitle key={index} eventTitle={item.title} number={index+1}/>
+    const usersEvent = event.map((item, index) => {
+        if (isSameDay(selected, parseISO(item.date))) {
+            return <EventTitle key={index} eventTitle={item.title} number={index + 1}/>
         }
     })
 
+    useEffect(() => {
+        fetch("/data.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setEvent(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
 
-    return (
-        <MyContext.Provider value={{selected, setSelected, month}}>
-        <div className={classes.calendar}>
-            <div className={classes.dateContainer}>
-                <div className={classes.time}>{format(startDay,'kk')}:{format(startDay,'mm')}:{format(startDay, 'ss')}</div>
-                <div className={classes.date}>{format(startDay,'EEEE')}, {format(startDay,'d')} {format(startDay,'MMMM')} {format(startDay,'y')}</div>
-            </div>
-            <div className={classes.dateAndSelectedMonthContainer}>
-                <div className={classes.doubleContainer}>
-                    <div className={classes.selectedMonth}>{format(month,'LLLL')} {format(month,'y')}</div>
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return (
+            <MyContext.Provider value={{selected, setSelected, month}}>
+                <div className={classes.calendar}>
+                    <div className={classes.dateContainer}>
+                        <div
+                            className={classes.time}>{format(startDay, 'kk')}:{format(startDay, 'mm')}:{format(startDay, 'ss')}</div>
+                        <div
+                            className={classes.date}>{format(startDay, 'EEEE')}, {format(startDay, 'd')} {format(startDay, 'MMMM')} {format(startDay, 'y')}</div>
+                    </div>
+                    <div className={classes.dateAndSelectedMonthContainer}>
+                        <div className={classes.doubleContainer}>
+                            <div className={classes.selectedMonth}>{format(month, 'LLLL')} {format(month, 'y')}</div>
+                            <div>
+                                <button style={{marginRight: 10}} onClick={prev}>&#5169;</button>
+                                <button onClick={next}>&#5167;</button>
+                            </div>
+                        </div>
+                        <Month/>
+                    </div>
+                    <EventList selectedDay={selected} createEvent={createEvent}/>
                     <div>
-                        <button style={{marginRight: 10}} onClick={prev}>&#5169;</button>
-                        <button onClick={next}>&#5167;</button>
+                        {
+                            usersEvent
+                        }
                     </div>
                 </div>
-                <Month/>
-            </div>
-                <EventList selectedDay={selected} createEvent={createEvent}/>
-            <div> {
-                usersByLikes
-            }
-            </div>
-        </div>
-        </MyContext.Provider>
-    );
+            </MyContext.Provider>
+        );
+    };
 };
 
 export default Calendar;
