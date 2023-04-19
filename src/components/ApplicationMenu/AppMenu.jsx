b1cc323423778f8ec62537069da0fc1694fbc4c2
Alexander <sumry.stereo123@ukr.net>
2023-04-19 17:14:47 +0300

    done application menu

--- src/components/ApplicationMenu/AppMenu.jsx
+++ src/components/ApplicationMenu/AppMenu.jsx
@@ -3,6 +3,8 @@ import classes from "./Menu.module.css";
 import CustomInput from "../UI/CustomInput/CustomInput";
 import nightModeImg from "../../images/moon.png"
 import dayModeImg from "../../images/sun.png"
+import userImg from "../../images/user.png"
+import calendarIMG from "../../images/calendar.png"
 import cl from "../Form/Form.module.css"
 
 const AppMenu = () => {
@@ -21,14 +23,16 @@ const AppMenu = () => {
 
     return (
         <div className={classes.appMenu}>
-            <div className={classes.search}>
-                <div>Icon</div>
+            <div className={classes.searchContainer}>
+                <div className={classes.paddingContainer}><img src={calendarIMG} alt="CalendarIcon"/></div>
                 <CustomInput/>
             </div>
-            <div className={classes.sign}>
-                <button>Sign In</button>
-                <button>Sign Up</button>
-                <div>Icon</div>
+            <div className={classes.signContainer}>
+                <div className={classes.sign}>
+                    <button className={classes.paddingContainer}>Sign In</button>
+                    <button className={classes.paddingContainer}>Sign Up</button>
+                    <div className={cl.image}><img src={userImg} alt="User"/></div>
+                </div>
                 <div>
                     <button className={cl.image} onClick={turnOnNightMode}><img src={nightModeImg} alt="Moon"/></button>
                     <button className={cl.image} onClick={turnOffNightMode}><img src={dayModeImg} alt="Sun"/></button>
